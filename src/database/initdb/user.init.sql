57ffee91288aa9c75f3eb19537691c85c4b6b133
zachrousseau <zachrousseau0@gmail.com>
2025-03-19 21:02:27 -0400

    Create db in docker

--- src/database/initdb/user.init.sql
+++ src/database/initdb/user.init.sql
@@ -1,3 +1,5 @@
+USE users;
+
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
@@ -9,6 +11,4 @@ INSERT INTO users (username, password) VALUES
     ('sheogorath', 'cheeeese'),
     ('talos', 'dragonborn4eva'),
     ('test_user', 'test_password');
-
-SELECT * FROM  users;
 
